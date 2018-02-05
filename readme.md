d6206ab89a51a29f22ee656aa5b67625d0410177
greenfrog <greenfrog82@naver.com>
2018-02-05 23:56:35 +0900

    Initial commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# logging_ex package
+
+Python의 기본 logging package를 확장한 확장패키지
+
+## Handlers
+
+### logging.handlers.RotatingFileHandlerEx
+
+logging.handlers.RotatingFileHandler를 확장한 클래스  
+로그파일의 사용자, 그룹 그리고 권한을 지정해줄 있다. 
+
+**Usage :** [ex_rotating_file_handler_ex.py](./src/ex_rotating_file_handler_ex.py)
+
---
+++ src/logging_ex/ex_rotating_file_handler_ex.py
@@ -0,0 +1,37 @@
+import logging
+import logging.config
+import logging_ex
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s %(levelname)s %(name)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandlerEx',
+            'formatter': 'default',
+            'filename': 'test.log',
+            'owner': ['greenfrog', 'admin'],
+            'chmod': 0660,
+            'maxBytes': 60,
+            'backupCount': 10,
+            # 'delay': True
+            'delay': False
+        }
+    },
+    'root': {
+        'handlers': ['file'],
+        'level': 'DEBUG',
+    },
+}
+
+logging.config.dictConfig(LOGGING)
+logger = logging.getLogger(__name__)
+
+for idx in range(0, 10):
+    logger.debug('%d > A debug message' % idx)
